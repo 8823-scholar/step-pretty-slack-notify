b550814d48a7320cff66d73d2698060b0c669104
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2014-11-25 03:12:41 +0900

    customizable messages.

--- run.rb
+++ run.rb
@@ -9,6 +9,10 @@ require "slack-notifier"
 webhook_url = ENV["WERCKER_PRETTY_SLACK_NOTIFY_WEBHOOK_URL"]
 channel     = ENV["WERCKER_PRETTY_SLACK_NOTIFY_CHANNEL"]
 username    = ENV["WERCKER_PRETTY_SLACK_NOTIFY_USERNAME"]
+build_success_message = ENV["WERCKER_PRETTY_SLACK_NOTIFY_BUILD_SUCCESS_MESSAGE"]
+build_failed_message = ENV["WERCKER_PRETTY_SLACK_NOTIFY_BUILD_FAILED_MESSAGE"]
+deploy_success_message = ENV["WERCKER_PRETTY_SLACK_NOTIFY_DEPLOY_SUCCESS_MESSAGE"]
+deploy_failed_message = ENV["WERCKER_PRETTY_SLACK_NOTIFY_DEPLOY_FAILED_MESSAGE"]
 
 abort "Please specify the your slack webhook url" unless webhook_url
 username = "Wercker"                              unless username
@@ -32,19 +36,23 @@ def deploy?
 end
 
 def build_message(app_name, app_url, build_url, git_commit, git_branch, started_by, status)
-  "[[#{app_name}](#{app_url})] [build(#{git_commit[0,8]})](#{build_url}) of #{git_branch} by #{started_by} #{status}"
+  m = status == "failed" ? build_failed_message : build_success_message
+  "#{m}\n[[#{app_name}](#{app_url})] [build(#{git_commit[0,8]})](#{build_url}) of #{git_branch} by #{started_by} #{status}"
 end
 
 def deploy_message(app_name, app_url, deploy_url, deploytarget_name, git_commit, git_branch, started_by, status)
-  "[[#{app_name}](#{app_url})] [deploy(#{git_commit[0,8]})](#{deploy_url}) of #{git_branch} to #{deploytarget_name} by #{started_by} #{status}"
+  m = status == "failed" ? build_failed_message : build_success_message
+  "#{m}\n[[#{app_name}](#{app_url})] [deploy(#{git_commit[0,8]})](#{deploy_url}) of #{git_branch} to #{deploytarget_name} by #{started_by} #{status}"
 end
 
 def icon_url(status)
-  "https://github.com/wantedly/step-pretty-slack-notify/raw/master/icons/#{status}.jpg"
+  #"https://github.com/wantedly/step-pretty-slack-notify/raw/master/icons/#{status}.jpg"
+  "https://s3-us-west-2.amazonaws.com/slack-files2/bot_icons/2014-11-12/2992212676_48.png"
 end
 
 def username_with_status(username, status)
-  "#{username} #{status.capitalize}"
+  username
+  #"#{username} #{status.capitalize}"
 end
 
 notifier = Slack::Notifier.new(
