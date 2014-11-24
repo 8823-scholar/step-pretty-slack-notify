bb5e5c533c9c639b8d1a7c1768df09d7f7a52a5f
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2014-11-25 03:18:39 +0900

    oops variable miss.

--- run.rb
+++ run.rb
@@ -41,7 +41,7 @@ def build_message(app_name, app_url, build_url, git_commit, git_branch, started_
 end
 
 def deploy_message(app_name, app_url, deploy_url, deploytarget_name, git_commit, git_branch, started_by, status)
-  m = status == "failed" ? build_failed_message : build_success_message
+  m = status == "failed" ? deploy_failed_message : deploy_success_message
   "#{m}\n[[#{app_name}](#{app_url})] [deploy(#{git_commit[0,8]})](#{deploy_url}) of #{git_branch} to #{deploytarget_name} by #{started_by} #{status}"
 end
 
